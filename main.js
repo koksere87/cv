ac2c7d9144792edb4bbfa2d89efb5380082dd3ad
koksere87 <81833015+koksere87@users.noreply.github.com>
2022-05-16 21:12:33 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const lines = document.querySelector(".lines");
+const nav = document.querySelector("nav");
+
+lines.addEventListener("click", function () {
+  nav.classList.toggle("activate");
+});
